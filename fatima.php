cb7ab38f2aa057d1b851df6b0058dfe0422c95e8
fatimaharunapapa <95175449+fatimaharunapapa@users.noreply.github.com>
2021-12-01 19:35:50 +0100

    Add files via upload

---
+++ fatima.php
@@ -0,0 +1,16 @@
+<html>
+<title> Bioinformatic Assignment </title>
+<?php 
+
+$_name = 'Fatima  Haruna  Fafa';
+$_Slack_id = '@fatima haruna fafa';
+$_email = 'fatimaharunafafa123@gmail.com';
+$_areaofinterest = 'Genomic';
+
+
+echo  '<p><h3>My Name is ',$_name, '</p></h3>';
+echo '<p><h3> My Slack Id is ',$_Slack_id, '</p></h3';
+echo  '<p><h3> My Email Address is ',$_email, '</p></h3';
+echo '<p><h3> I am very Interested in the field of  ',$_areaofinterest, '</p></h3';
+?>
+</html>
