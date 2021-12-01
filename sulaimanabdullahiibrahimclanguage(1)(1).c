c83cabc1e9c300930485d62e9192c8de6303ee3c
Sulaiman001a <95119870+Sulaiman001a@users.noreply.github.com>
2021-12-01 17:47:38 +0100

    Add files via upload

---
+++ sulaimanabdullahiibrahimclanguage(1)(1).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*Programme to print out my details
+    Written by:
+        SULAIMAN ABDULLAHI IBRAHIM
+        BASUG/UG/BHS/ANT/ANA/18/121
+
+    */
+
+
+
+    printf("BIO INFORMATICS");
+    printf("NAME: SULAIMAN ABDULLAHI IBRAHIM\n");
+    printf("EMAIL: sulaimanibrahimabdulmdr@gmail.com\n");
+    printf("SLACK USERNAME: @ISULAIMAN A IBRAHIM\n");
+    printf("GITHUB USERNAME: sulaiman001a\n");
+    printf("AREA OF INTEREST: GENOMICS\n");
+    return 0;
+}
