5f50d31b977887fb12fd53e870c649a9dd0004fa
Bilkisu Ahmad <95380099+Bikeesu@users.noreply.github.com>
2021-12-01 22:30:00 +0100

    Add files via upload

---
+++ Script_Balkisu.java
@@ -0,0 +1,11 @@
+package com.balkisu;
+
+public class ScriptBalkisu {
+
+  public static void main(String[] args) {
+   System.out.println("Name: BALKISU AHMAD");
+   System.out.println("Email: balkisuahmad222@gmail.com");
+   System.out.println("Slack Id: @Balkisu Ahmad");
+   System.out.println("Area of Interest: System Biology");
+  }
+}
