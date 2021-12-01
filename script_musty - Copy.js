42a8ba123898e5e359a782b505fa464ef4f5fb6b
mustaphayusufmuhammad <95125323+mustaphayusufmuhammad@users.noreply.github.com>
2021-12-01 15:02:10 +0100

    Add files via upload

---
+++ script_musty - Copy.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env
+alert('Name: Mustapha yusuf muhammad');
+alert('Email: mustyyusufmuhammad@gmail.com');
+alert('Area of interest: system biology');
+alert('Slack ID: @mustaphaymuhammad');
