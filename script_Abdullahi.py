9440bbb2dac5ebb51b424f0e5cdf7168a2c6991f
Apdoulahae <95097878+Apdoulahae@users.noreply.github.com>
2021-12-02 07:45:25 +0100

    Add files via upload

---
+++ script_Abdullahi.py
@@ -0,0 +1,8 @@
+#<-----------Abdullahi Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Abdullahi auwal garba, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Abdullahi Auwal Garba")
+print("Email: " + "\t\t\t"+ "abdullahiauwal796@gmail.com")
+print("Slack_username: " + "\t" + "@apdoulahae_Mr_Bee")
+print("Area of interest: " + "\t" + "Proteomics")
