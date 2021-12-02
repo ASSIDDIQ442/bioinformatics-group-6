263e86d7e1a2eb422d63c416d7fa5da3e47b9376
Saifullahmuhammadshehu <95107852+Saifullahmuhammadshehu@users.noreply.github.com>
2021-12-02 08:10:41 +0100

    Add files via upload

---
+++ script saifullah.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
+    cout << "\t\tHello saifullah muhammad shehu:\n\n";
+    cout << "Name:"<< "\t\t\t" << "saifullah muhammad shehu\n";
+    cout << "Email:"<< "\t\t\t" << "saifullahmuhd125@gmail.com\n";
+    cout << "Slack_Id:" << "\t\t" << "@muhammad saifullah shehu\n";
+    cout << "Area Of Interest:" << "\t" << "Metagenomics\n";
+    return 0;
+}
