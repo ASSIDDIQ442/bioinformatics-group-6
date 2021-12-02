f48b55bc134baa4384ee780dc3a5cdacbfbfcfb1
Siyamah2000 <95225932+Siyamah2000@users.noreply.github.com>
2021-12-02 13:23:40 +0100

    Add files via upload

---
+++ Script_Siyamah.py
@@ -0,0 +1,8 @@
+#<-----------kaltum Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Haladu Garba Kaltum, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Haladu Garba Kaltum")
+print("Email: " + "\t\t\t"+ "ummikursumhaladu@gmail.com")
+print("Slack_username: " + "\t" + "@Haladu_G_Kaltume")
+print("Area of interest: " + "\t" + "Genomics")
